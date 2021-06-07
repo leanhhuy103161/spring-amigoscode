dfb6746041b31c1dbe9997a9403717f1883455ef
leanhhuy103161 <leanhhuy103161@gmail.com>
2021-06-07 15:43:43 +0700

    add spring security into spring project

---
+++ src/main/java/com/amigoscode/basicspring/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.amigoscode.basicspring.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/amigoscode/basicspring/registrantion/RegistrationService.java
@@ -0,0 +1,11 @@
+//package com.amigoscode.basicspring.registrantion;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class RegistrationService {
+//
+//    public String register(RegistrationRequest request) {
+//        return "worked";
+//    }
+//}
